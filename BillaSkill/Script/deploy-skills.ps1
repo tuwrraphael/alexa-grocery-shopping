cd79e240381b2c77ec54c1ebe8fde6c26df785df
Raphael <raphael.hauk@racing.tuwien.ac.at>
2018-01-08 03:30:34 +0100

    deployment script

---
+++ BillaSkill/Script/deploy-skills.ps1
@@ -0,0 +1,15 @@
+﻿function Deploy($Profile, $Skillname){
+	ask clone -p $Profile
+	$file1 = "./" + $Skillname + "/.ask/config"
+	$file2 = "./.ask/config"
+	$opts = @{ Path = $file1
+	Destination = $file2  }
+	Move-Item -Force @opts
+	rm -Force -Recurse ./$Skillname
+	ask deploy -p $Profile -t model
+	git checkout ./.ask/config
+}
+cd ../Skill
+Deploy -Profile andreas -Skillname billa
+Deploy -Profile default -Skillname Billa
+cd ../Script
